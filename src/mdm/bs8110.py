c63dd37f2d425e73e1b5bb74714a2b95b77f3d26
akins-dev <bobemmanuel503@gmail.com>
2026-07-06 10:27:20 +0100

    chore: delete empty files

--- src/mdm/bs8110.py
+++
@@ -1,1 +0,0 @@
-# This file has been moved to src/mdm/design/bs8110.py
--- src/mdm/core.py
+++
@@ -1,3 +0,0 @@
-# This file has been moved. 
-# Beam models are in src/mdm/models/beam.py
-# MDM solver logic is in src/mdm/analyzers/continuous.py
